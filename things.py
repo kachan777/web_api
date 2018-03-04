53776c2cbe38fd56d8fce137f56ec3da72b0fe17
Takeshi Katsumata <katsumata@PC0034.local>
2018-03-04 23:03:56 +0900

    first commit

---
+++ things.py
@@ -0,0 +1,16 @@
+import falcon
+
+class ThingsResource(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = ('\nTwo things awe me most, the starry sky '
+                     'above me and the moral law within me.\n'
+                     '\n'
+                     '    ~ Immanuel Kant\n\n')
+
+app = falcon.API()
+
+things = ThingsResource()
+
+app.add_route('/things', things)
